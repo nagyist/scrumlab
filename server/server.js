04e69e8babbb37ee047ba71d5560b20603bdd1a4
Sebastian Sebald <sebastian.sebald@gmail.com>
2013-08-16 13:52:53 +0200

    improved node server

--- server/server.js
+++ server/server.js
@@ -8,22 +8,30 @@ var config = require('./config.js'),
 	protectJSON = require('./lib/protectJSON');
 
 var app = express(),
-	server = http.createServer(app);
+	server = http.createServer(app),
+	sessions = {};
 
 var gitlab = require('./lib/gitlab/gitlab.js')( config.gitlab );
 
 // Configuration
 // -------------------------
 app.configure(function () {
-	app.use(express.logger());
+	app.use(express.logger({ format: ':method :url' }));
 	app.use(express.bodyParser());
 
 	app.use(express.cookieParser(config.server.secret));
 	app.use(express.session());
 	app.use(app.router);
-	app.use(express.logger());
 
 	app.use(protectJSON);
+
+	// Send Headers.
+	app.use(function ( req, res, next ) {
+		res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+		res.setHeader('Pragma', 'no-cache');
+		res.setHeader('Expires', '0');
+		return next();
+	});
 });
 
 
@@ -40,13 +48,15 @@ server.listen(config.server.listenPort, 'localhost', 511, function() {
 // Authentication
 // -------------------------
 app.post('/api/login', function ( req, res ) {
-	console.log(req.session);
-
+	console.log('login');
 	gitlab.login(req.body.email, req.body.password, function ( body ) {
 		if( body.username ) {
-			req.session.user = body.username;
+			req.session.user = { 'name': body.username, 'id': body.id };
 		}
-
+		if( body.private_token ) {
+			sessions[body.id] = body.private_token;
+			delete body.private_token;
+		}
 		res.send( body );
 	});
 });
@@ -54,10 +64,19 @@ app.post('/api/login', function ( req, res ) {
 
 // Projects
 // -------------------------
-app.get('/api/projects', function ( req, res ) {
-	gitlab.projects.all(function ( status, body ) {
-		res.send( status, body );
-	});
+app.get(/\/api\/.*/, function ( req, res ) {
+	var token;
+	// console.log(req.url);
+	// console.log(req.method);
+	try {
+		token = sessions[req.session.user.id];
+		gitlab.projects.all( token, function ( status, body ) {
+			res.send( status, body );
+		});
+	} catch (e) {
+		console.log('WARNING: Unautorized Request for "' + req.path + '".');
+		res.send( 401, 'Unauthorized.' );
+	}
 });
 
 
@@ -70,6 +89,7 @@ app.get(/\.html$|\.js$|\.css$/, function (req, res ) {
 
 // This route enables HTML5Mode by forwarding missing files to the index.html
 app.all('/*', function(req, res) {
+	console.log('static');
 	// Just send the index.html for other files to support HTML5Mode
 	res.sendfile('index.html', { root: config.server.distFolder });
 });
