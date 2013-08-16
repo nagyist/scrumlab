cfece8f6750727328e272320a977d9c13e3153dd
Sebastian Sebald <sebastian.sebald@gmail.com>
2013-08-16 15:53:26 +0200

    push a request to retry qeueue

--- client/src/components/auth/interceptor.js
+++ client/src/components/auth/interceptor.js
@@ -1,14 +1,15 @@
-angular.module('auth.interceptor', [])
+angular.module('auth.interceptor', ['auth.retryQueue'])
 
 // Service to intercept authentication failures.
 // If an authentiation failure occurs it will store the request in an queue
 // and redirect the user to the login page.
-.factory( 'authInterceptor', [ '$location', function ( $location ) {
+.factory( 'authInterceptor', [ '$q', '$location', 'authRetryQueue', function ( $q, $location, queue ) {
 	return function ( promise ) {
 		// Intercept failed requests.
 		return promise.then( null, function ( response ) {
 			if( response.status === 401 ) {
-				// TODO: add retry queue.
+				var deferred = $q.defer();
+				queue.push( response.config, deferred );
 				// Redirect to login page.
 				$location.path('/login');
 			}
