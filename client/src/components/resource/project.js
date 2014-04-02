7a810826bdf089f63b803cadba6856d72a9faa22
Sebastian Sebald <sebastian.sebald@gmail.com>
2014-04-02 14:20:13 +0200

    commit something
    pups i forget something

--- client/src/components/resource/project.js
+++ client/src/components/resource/project.js
@@ -1,5 +1,8 @@
 angular.module('scrumlab.resouce.project', ['ngResource'])
 .factory( 'Project', [ '$resource', function ( $resource ) {
 	var Project = $resource('/api/projects/:id', { id: '@id' });
+
+	console.log("this is a change");
+
 	return Project;
 }]);
